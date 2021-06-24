dff3ea0619340db96e04fe6236b926f78cd3283e
Ryan Krenzke <ryan.krenzke@gmail.com>
2021-06-23 20:44:41 -0400

    edituser skeleton

---
+++ src/components/Users/EditUser.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './EditUser.module.css';
+
+const EditUser = (props) => {
+    return <div>This is the Edit User component</div>
+}
+
+export default EditUser;
